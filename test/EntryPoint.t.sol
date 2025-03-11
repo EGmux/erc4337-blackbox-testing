65993be96d74e68f9e5292ba9e109e22d1fc876b
Enzo Gurgel <egb2@localhost.localdomain>
2025-03-11 11:37:28 -0300

    update: EntryPoint.t.sol imports

--- test/EntryPoint.t.sol
+++ test/EntryPoint.t.sol
@@ -3,39 +3,29 @@ pragma solidity ^0.8.23;
 import "../src/core/EntryPoint.sol";
 import "../src/interfaces/PackedUserOperation.sol";
 import {Test} from "forge-std/Test.sol";
+import "./utils/TestUserOperations.sol";
+import "./utils/PartialUserOperation.sol";
 
-struct partialUserOp {
-    address sender;
-    uint256 nonce;
-    // bytes initCode;
-    // bytes callData;
-    bytes32 accountGasLimits;
-    uint256 preVerificationGas;
-    bytes32 gasFees;
-    // bytes paymasterAndData;
-    // bytes signature;
-}
 
 contract EntrypointBoundary is Test{
 	EntryPoint entryPoint;
+	Utils ut;
 
 
 	function setUp() public {
 		
 		 entryPoint = new EntryPoint();
+		 ut = new Utils();
 
 	}
 
-	function proveFail_CallDataZeroAddressSender(partialUserOp memory puop, address payable beneficiary) public {
-	    require(puop.sender == address(0), "Invalid arguments!");
+	function proveFail_CallDataZeroAddressSender(PartialUserOperation memory partialUserOp, address payable beneficiary) public {
+	    require(partialUserOp.sender == address(0), "Invalid arguments!");
             
 	    PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
-	    userOps[0].sender = puop.sender;
-	    userOps[0].nonce = puop.nonce;
-	    userOps[0].accountGasLimits = puop.accountGasLimits;
-	    userOps[0].preVerificationGas = puop.preVerificationGas;
-	    userOps[0].gasFees = puop.gasFees;
-	    entryPoint.handleOps(userOps, beneficiary);
-    
+
+	    userOps[0] = ut.convertPartialUserOperationToUserOperation(partialUserOp,userOps[0]);
+		
+    	    entryPoint.handleOps(userOps, beneficiary);
   }
 }
