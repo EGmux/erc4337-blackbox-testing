e1573e7e53a9f9f08808f2f944b7215f1916359f
Enzo Gurgel <egb2@localhost.localdomain>
2025-03-11 17:22:40 -0300

    update: TestUserOperations.sol

--- test/utils/TestUserOperations.sol
+++ test/utils/TestUserOperations.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.23;
 import "../../src/interfaces/PackedUserOperation.sol";
 import "./PartialUserOperation.sol";
-
+import {Vm} from "forge-std/Vm.sol";
 
 contract Utils{
 
@@ -14,9 +14,13 @@ contract Utils{
 
 		    packed.sender = partialUserOp.sender;
 		    packed.nonce = partialUserOp.nonce;
+		    // packed.initCode = bytes(0);
+		    // packed.callData = bytes(0);
 		    packed.accountGasLimits = partialUserOp.accountGasLimits;
 		    packed.preVerificationGas = partialUserOp.preVerificationGas;
 		    packed.gasFees = partialUserOp.gasFees;
+		    // packed.paymasterAndData = bytes(0);
+		    // packed.signature = key;
 		    return packed;
 		}
 
@@ -29,9 +33,13 @@ contract Utils{
 		for (uint256 i = 0; i < numOfOps; ++i){
 		    packed[i].sender = partialUserOp[i].sender;
 		    packed[i].nonce = partialUserOp[i].nonce;
+		    // packed[i].initCode = bytes(0);
+		    // packed[i].callData = bytes(0);
 		    packed[i].accountGasLimits = partialUserOp[i].accountGasLimits;
 		    packed[i].preVerificationGas = partialUserOp[i].preVerificationGas;
 		    packed[i].gasFees = partialUserOp[i].gasFees;
+		    // packed[i].paymasterAndData = bytes(0);
+		    // packed[i].signature = bytes(0);
 		}
 
 		    return packed;
