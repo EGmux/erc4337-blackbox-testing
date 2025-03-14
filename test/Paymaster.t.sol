7a512cf3286d0094e173da75243f26befda191d6
Enzo Gurgel <egb2@localhost.localdomain>
2025-03-14 08:02:41 -0300

    update: add Paymaster.t.sol - proveFail_stakeCantBeZero

--- test/Paymaster.t.sol
+++ test/Paymaster.t.sol
@@ -38,6 +38,39 @@ contract PaymasterTest {
 	}
 
 	
+    // /// @inheritdoc IStakeManager
+    // function addStake(uint32 unstakeDelaySec) external payable {
+    //     DepositInfo storage info = deposits[msg.sender];
+    //     require(unstakeDelaySec > 0, "must specify unstake delay");
+    //     require(
+    //         unstakeDelaySec >= info.unstakeDelaySec,
+    //         "cannot decrease unstake time"
+    //     );
+    //     uint256 stake = info.stake + msg.value;
+    //     require(stake > 0, "no stake specified");
+    //     require(stake <= type(uint112).max, "stake overflow");
+    //     deposits[msg.sender] = DepositInfo(
+    //         info.deposit,
+    //         true,
+    //         uint112(stake),
+    //         unstakeDelaySec,
+    //         0
+    //     );
+    //     emit StakeLocked(msg.sender, stake, unstakeDelaySec);
+    // }
+    //
 
+    // prove stake can't be zero wei
+    function proveFail_stakeCantBeZero(address account, uint256 dealt, uint256 deposit, uint32 delay, uint112 staked) public {
+		
+		// https://docs.soliditylang.org/en/latest/types.html#address
+		require(account != address(type(uint160).max + 1) && account != address(0));
+		require(deposit <= dealt && staked == uint112(0));
 
+		vm.deal(account, dealt wei);
+
+		entryPoint.despositTo{value: deposit}(account);
+
+		entryPoint.addStake{value: staked}(delay);
+    }
 }
